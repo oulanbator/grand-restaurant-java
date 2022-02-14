383d90b26f3a1b0ffc5228155cce25d33e87ca8b
Victor Matheron <victor.matheron@gmail.com>
2022-02-14 21:10:15 +0100

    Trying to trigger test bugs in sonarqube - Add while true loop in main

--- src/main/java/epsi/tdd/grandrestaurant/GrandRestaurantApplication.java
+++ src/main/java/epsi/tdd/grandrestaurant/GrandRestaurantApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GrandRestaurantApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GrandRestaurantApplication.class, args);
-		System.out.println("DEMARRE");
+		while(true) {
+
+		}
+		// SpringApplication.run(GrandRestaurantApplication.class, args);
+		// System.out.println("DEMARRE");
 	}
 
 }
